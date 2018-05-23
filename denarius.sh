10a1565ae97f68fb0a78db7b7a7e22415bd3bdd3
buzzkillb <travanx@gmail.com>
2018-05-23 09:19:56 -0700

    6) masternode status

--- denarius.sh
+++ denarius.sh
@@ -10,6 +10,7 @@ do
   echo "Enter 3 to Install Pre-Compiled Binary: "
   echo "Enter 4 to Getinfo: "
   echo "Enter 5 to scroll debug.log: "
+  echo "Enter 6 to Masternode stats: "
   echo "Enter q to exit this menu: "
   echo -e "\n"
   echo -e "Enter your selection \c"
@@ -20,6 +21,7 @@ do
     3) bash -c "$(wget -O - https://raw.githubusercontent.com/buzzkillb/DNR-masternode/master/nocompile.sh)" ;;
     4) denariusd getinfo ;;
     5) tail -f ~/.denarius/debug.log ;;
+    6) denariusd masternode status ;;
     q) exit ;;
   esac
   echo -e "Enter return to continue \c"
